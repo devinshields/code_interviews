deb8feba5c084324b03ab85a2d9fa7303f95a2cf
devin shields <devin.shields@gmail.com>
2014-10-05 18:22:54 -0400

    added hard problems from the google interviewer guy, some union find implementation notes

---
+++ datastructures/weightedunionfind.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+'''
+http://algs4.cs.princeton.edu/15uf/WeightedQuickUnionUF.java.html
+
+TODO - translate to python
+
+'''
+
+
---
+++ hard_problems/hard_problems.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+'''
+http://www.reddit.com/r/cscareerquestions/comments/1z97rx/from_a_googler_the_google_interview_process
+
+python weighted union-find:
+  http://blog.singhanuvrat.com/problems/union-find-data-structure-in-python
+
+java weighted union-find:
+  http://algs4.cs.princeton.edu/15uf/WeightedQuickUnionUF.java.html
+'''
+
+
+# remove duplicates from a list of strings which is larger than the available memory (i.e. with reloads from disk)
+# implement a program which plays tic-tac-toe
+
+
+# count the number of disjoint objects in a bitmap
+
+import random;random.seed(42)
+
+N=8
+bitmap = [[random.choice([0, 1]) for x in range(N)] for y in range(N)]
+
+  
+
+
+
+
+print
+for row in bitmap:
+  print row
+print
+
+
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
+
