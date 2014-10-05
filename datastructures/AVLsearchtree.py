8c337efd21ae097f841e736a680a0a2133155038
devin shields <devin.shields@gmail.com>
2014-10-05 04:59:10 -0400

    added more notes an a link to some AVL tree implementation stuff

---
+++ datastructures/AVLsearchtree.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+http://interactivepython.org/courselib/static/pythonds/Trees/balanced.html
+'''
+
+
+
+def test():
+  '''  '''
+
+
+if __name__ == '__main__':
+  test()
+
