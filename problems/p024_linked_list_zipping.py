2fbce3a0ac569f488cef3bfc36c471b553cf2d72
devin shields <devin.shields@gmail.com>
2014-10-05 14:15:21 -0400

    close enough on the list zipping thing. need to work on my pointer skills

--- problems/p024_linked_list_zipping.py
+++ problems/p024_linked_list_zipping.py
@@ -24,66 +24,33 @@ TRADEOFFS:        x)
 ---------------------------------------------------
 SOLUTIONS:
 ---------------------------------------------------
-
-BRUTE FORCE:      x) make a generating function that produces the indices that need swapping
-                  x) feed each index 2-tuple to a swap function, iterating over the list for each swap
-
-ANY PATTERNS?:    x)
-
-REFINED:          x) forward iterate over the list once and
-                     use loop counters to calculate link distance to the next swap node.
-                     continue until hitting the half way point node.
-
-EVEN BETTER:      x) find the middle of the linked list - save it
-                  x) reverse the pointers of the second half of the list
-                       save a ref to the tail node, it's now a 'second' head
-                       now there's a weird, 2 branched tree pointing at the middle of the list
-                  x) iterate from both heads, changing pointers and doing crazy shit until you hit the central node
-
-CASES:            x) even # of nodes
-                  x) odd # of nodes
-
-                  x) example: input  - (0, 1, 2, 3, 4)
-                              inter  - (0, 1, 2)
-                                        (4, 3)
-                              output - (0, 4, 1, 3, 2)
-
-                              input  - (0, 1, 2, 3, 4, 5)
-                              inter  - (0, 1, 2, 3)
-                                        (5, 4)
-                              output - (0, 5, 1, 4, 2, 3)
-
-CAVEATS:          x)
-
----------------------------------------------------
-
 '''
 
-
-# import linked list
-class Node(object):
-  def __init__(self, cargo=None, next_node=None):
-    self.cargo = cargo
-    self.next_node  = next_node
-  def __str__(self):
-    return 'Node({0})'.format(str(self.cargo))
-
-
 class LinkedList(object):
+  ''' '''
+  class Node(object):
+    def __init__(self, cargo=None, next_node=None):
+      self.cargo = cargo
+      self.next_node  = next_node
+    def __str__(self):
+      return 'Node({0})'.format(str(self.cargo))
+
   def __init__(self, collection=None):
     self.head = None
     # build from a source collections
     if collection:
       # build, then link nodes
-      nodes = [Node(x) for x in collection]
+      nodes = [self.Node(x) for x in collection]
       for p0, p1 in zip(nodes[:-1], nodes[1:]):
         p0.next_node = p1
       self.head = nodes[0]
+
   def as_generator(self):
     current_node = self.head
     while current_node:
       yield current_node
       current_node = current_node.next_node
+
   def __str__(self):
       return '[{0}]'.format(', '.join(map(str, self.as_generator())))
 
@@ -91,17 +58,60 @@ class LinkedList(object):
 
 
 # test data
-arr = range(6)
-link_list = LinkedList(arr)
-
-print '\ntest data:', link_list, '\n'
+arr = range(5)
+llist = LinkedList(arr)
+print '\n', llist, '\n'
 
 
 # IMPLEMENTATION
 
+import itertools
+
+def zip_linked_list(linked):
+  '''  '''
+  # iterate over the list to get the middle and last elements
+  middle, last = linked.head, linked.head
+  for i in itertools.count():
+    print '\t', i, middle, last
+    # break the loop when we hit the end
+    if not last.next_node:
+      break
+    # otherwise advance the middle and last pointers, slow and fast respectively
+    last = last.next_node
+    if i % 2:
+      middle = middle.next_node
+
+  # starting at middle, reverse the node pointers
+  zipper_prior, zipper_node = middle, middle.next_node
+  
+  # middle is the new tail when the list is odd length
+  middle.next_node = None
+  while zipper_node:
+    print '\t', zipper_node
+    # save a ref to the 'real' next node
+    real_next = zipper_node.next_node
+    zipper_node.next_node = zipper_prior
+    zipper_node = real_next
+
+  # start zippering
+  first = linked.head
+  while not first == middle:
+    second = first.next_node
+    tail_second = last.next_node
+    # update the various node pointers
+    first.next_node, last.next_node = last, second
+    # iterate over the list
+    first, last = second.next_node, tail_second
+  return linked
+
+
+
 # TEST DATA
+print zip_linked_list(llist)
+
 
 # TEST RUN
+
 
 # EYEBALL SOLUTION
 
