d23351fc313c643e2e42cd9b0f34d45c541fa2b2
devin shields <devin.shields@gmail.com>
2014-10-05 06:10:39 -0400

    added a real, in place quick sort implementation with detailed comments

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -1,8 +1,67 @@
 #!/usr/bin/env python
 '''
-notes:  https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot
-        https://en.wikipedia.org/wiki/Quicksort#Optimizations
+https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot
+https://en.wikipedia.org/wiki/Quicksort#Optimizations
+http://stackoverflow.com/questions/17773516/in-place-quicksort-in-python
+
+
+  - in the fancy, O(1) space complexity version, the 
+        
 '''
+
+
+import random
+
+
+def sub_partition(array, start, end, idx_pivot):
+    ''' returns the position where the pivot winds up
+        TODO: rename/reformat to match wikipedia's names - https://en.wikipedia.org/wiki/Quicksort#Algorithm '''
+
+    if not (start <= idx_pivot <= end):
+        raise ValueError('idx pivot must be between start and end')
+
+    # swap the pivot value with the first value the subarray of interest (start, end)
+    array[start], array[idx_pivot] = array[idx_pivot], array[start]
+
+    # track the pivot value
+    pivot = array[start]
+    
+    # i is the storeIndex, j is the loopIndex. note that i <= j, always.
+    i = start + 1
+    j = start + 1
+
+    # TODO: use xrange
+    while j <= end:
+        # if the loop index value is less than the pivot value,
+        # move the loop value down into the less_than region
+        if array[j] <= pivot:
+            array[j], array[i] = array[i], array[j]
+            # advance the border of the less_than region
+            i += 1
+        # advance the low-value-seracher index
+        j += 1
+
+    # move the partition value where it belongs
+    # the top of the less_than region
+    array[start], array[i - 1] = array[i - 1], array[start]
+
+    # let the parent function call know exactly where
+    # the partial ordering vs. pivot holds
+    return i - 1
+
+
+def quicksort(array, start=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    if end - start < 1:
+        return
+    
+    idx_pivot = random.randint(start, end)
+    i = sub_partition(array, start, end, idx_pivot)
+    
+    quicksort(array, start, i - 1)
+    quicksort(array, i + 1, end)
+    return array
 
 
 def quicksort_recursive(arr):
@@ -47,11 +106,13 @@ def test():
   a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
   a0, a1 = tuple(a), tuple(sorted(a))
 
-  #
-  assert tuple(quicksort_recursive(a)) == a1
+  
 
-  assert tuple(quicksort_recursive_list_comprehension(a)) == a1
+  assert tuple(quicksort_recursive(list(a0))) == a1
+  assert tuple(quicksort_recursive_list_comprehension(list(a0))) == a1
+  assert tuple(quicksort(list(a0))) == a1
 
+  
 
   #
   import __main__
