c2d9f50316b49608c08a65d39e93330bb1fc7ee2
devin shields <devin.shields@gmail.com>
2014-10-08 23:06:09 -0400

    added memorization example to the utilities library

---
+++ utilities/memoize_example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''  '''
+
+def memoize(f):
+  ''' a simple memoization decorator '''
+  cache = {}
+  def memf(*x):
+    if x not in cache:
+      cache[x] = f(*x)
+    return cache[x]
+  return memf
+
+
+
+@memoize
+def fib(n):
+  if n <= 2:
+    return 1
+  else:
+    return fib(n-1) + fib(n-2)
+
+
+
+def test():
+
+  print fib(100)
+
+if __name__ == '__main__':
+  test()
+
